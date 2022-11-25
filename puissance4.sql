24adc96c135c6ca87f31c91750991939299df610
sylvian <101133526+sylv0r@users.noreply.github.com>
2022-11-25 14:33:20 +0100

    bdd

--- puissance4.sql
+++ puissance4.sql
@@ -1,10 +1,10 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : mar. 15 nov. 2022 à 12:54
--- Version du serveur :  5.7.34
+-- Généré le : ven. 25 nov. 2022 à 13:32
+-- Version du serveur : 5.7.34
 -- Version de PHP : 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -58,7 +58,7 @@ CREATE TABLE `game` (
 --
 
 INSERT INTO `game` (`id`, `game_name`) VALUES
-(1, 'The Power Of Memory');
+(1, 'Power of Memory');
 
 -- --------------------------------------------------------
 
@@ -79,11 +79,12 @@ CREATE TABLE `message` (
 --
 
 INSERT INTO `message` (`id`, `id_game`, `id_user`, `message`, `date_message`) VALUES
-(1, 1, 3, 'J\'ai envoyé un message', '2022-11-13 20:36:07'),
-(2, 1, 5, 'Moi aussi', '2022-11-11 19:36:40'),
-(3, 1, 3, 'Ce message a été envoyé le 12 novembre 2022', '2022-11-12 09:11:49'),
-(4, 1, 4, 'Hello', '2022-11-14 19:55:29'),
-(5, 1, 3, 'Ce message est plus vieux que l\'autre', '2022-11-14 16:46:32');
+(173, 1, 61, 'test', '2022-11-25 12:47:46'),
+(174, 1, 3, 'BONJOUR', '2022-11-25 12:54:40'),
+(175, 1, 62, 'fqefef', '2022-11-25 13:17:41'),
+(176, 1, 61, 'azazd', '2022-11-25 13:19:23'),
+(177, 1, 61, 'azdazd', '2022-11-25 13:19:43'),
+(178, 1, 61, 'azdazdazd', '2022-11-25 13:20:46');
 
 -- --------------------------------------------------------
 
@@ -105,17 +106,16 @@ CREATE TABLE `score` (
 --
 
 INSERT INTO `score` (`id`, `id_user`, `id_game`, `id_difficulty`, `score`, `date_game`) VALUES
-(1, 3, 1, 1, 30, '2022-11-03 15:41:23'),
-(2, 3, 1, 3, 56, '2022-11-04 08:08:55'),
-(3, 5, 1, 1, 18, '2022-11-04 08:15:40'),
-(4, 5, 1, 4, 127, '2022-11-04 08:15:53'),
-(5, 4, 1, 2, 34, '2022-11-04 08:16:05'),
-(6, 4, 1, 1, 3, '2022-11-04 08:16:28'),
-(7, 5, 1, 2, 22, '2022-11-10 08:25:25'),
-(8, 3, 1, 4, 355, '2022-11-10 08:25:25'),
-(9, 4, 1, 3, 87, '2022-11-10 08:26:07'),
-(10, 5, 1, 1, 2, '2022-11-10 08:26:07'),
-(12, 5, 1, 3, 45, '2022-11-10 08:28:54');
+(129, 43, 1, 1, 999, '2022-11-24 20:52:15'),
+(130, 61, 1, 3, 61, '2022-11-25 09:24:11'),
+(131, 61, 1, 1, 44, '2022-11-25 09:29:44'),
+(132, 61, 1, 1, 77, '2022-11-25 10:04:31'),
+(133, 61, 1, 1, 10, '2022-11-25 10:10:04'),
+(134, 61, 1, 1, 25, '2022-11-25 10:14:55'),
+(135, 61, 1, 1, 24, '2022-11-25 10:25:51'),
+(136, 61, 1, 1, 53, '2022-11-25 13:27:41'),
+(137, 62, 1, 1, 65, '2022-11-25 14:16:22'),
+(138, 62, 1, 1, 33, '2022-11-25 14:30:03');
 
 -- --------------------------------------------------------
 
@@ -137,9 +137,14 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `email`, `password`, `pseudo`, `date_inscription`, `date_last_connexion`) VALUES
-(3, 'bvitour2004@gmail.com', 'p4s5W0rD', 'Basvit', '2022-11-03 15:28:26', '2022-11-14 09:16:03'),
-(4, 'sylvian@gmail.com', '\"\'!ryhdp0', 'SylvOr', '2022-11-04 08:13:59', '2022-11-04 08:13:59'),
-(5, 'florian@gmail.com', '\"!rèfyh\'t54', 'Florianrcht', '2022-11-04 08:14:33', '2022-11-04 08:14:33');
+(3, 'bvitour2004@gmail.com', 'p4s5W0rD', 'Basvit', '2022-11-03 15:28:26', '2022-11-03 16:28:26'),
+(4, 'sylvian.vidal95@gmail.com', 'A1aaaaaaaa@', 'SylvOr', '2022-11-04 08:13:59', '2022-11-11 18:21:27'),
+(43, 'aaa@gmail.com', '96c812ce0df00c578f05f3029085165f194ae2c7ee99174b3afaab8935411221', 'lzefnlzkn', '2022-11-16 09:55:20', '2022-11-16 09:55:20'),
+(57, 'sylvian.zefzfzvidal95@gmail.com', '2f54e2ca52b8109372f7de594164ed15418c27df7f7f9f403541839a9e4ee175', 'aaaaa', '2022-11-16 15:21:34', '2022-11-16 15:21:34'),
+(59, 'sylvian.vidal95@gmail.com', '69065eb716831d15a31157e688c2bdf83b386fd7ae7515b18f350f90edfa9116', 'abcd', '2022-11-16 20:28:27', '2022-11-16 21:28:36'),
+(60, 'sylvi95@gmail.com', '4685aa187b573198e3175d1f6fa7e8c9ac8340c8f2fb25901ba4be6a430c9de0', 'phppro', '2022-11-24 09:52:32', '2022-11-24 19:21:28'),
+(61, 'abcd@gmail.com', '96c812ce0df00c578f05f3029085165f194ae2c7ee99174b3afaab8935411221', 'abcdefg', '2022-11-24 19:36:14', '2022-11-25 14:19:13'),
+(62, 'test@gmail.com', 'b6a6fa2364ce1d37898a06c32f092da1a3b63dcf8331fdbf3c03e8f079e16ee5', 'test', '2022-11-25 13:10:44', '2022-11-25 14:22:10');
 
 --
 -- Index pour les tables déchargées
@@ -200,19 +205,19 @@ ALTER TABLE `game`
 -- AUTO_INCREMENT pour la table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=179;
 
 --
 -- AUTO_INCREMENT pour la table `score`
 --
 ALTER TABLE `score`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=139;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- Contraintes pour les tables déchargées
